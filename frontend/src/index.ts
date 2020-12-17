6c7b28b34788e22aa7014c5a864260c83deeb785
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2020-12-18 01:02:22 +0700

    Wave animation

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,1 +1,11 @@
 import './styles/style.scss';
+
+window.addEventListener("load", function() {
+    let svgObject = (document.getElementById('svg-object') as HTMLEmbedElement)
+    // let svgContainer = document.getElementById('svg-container')
+    // let innerHTMLSVGObject = svgObject.getSVGDocument().querySelector('svg').innerHTML
+    // svgContainer.innerHTML = innerHTMLSVGObject
+    // svgObject.remove()
+    // svgObject.style.display = 'none'
+})
+
