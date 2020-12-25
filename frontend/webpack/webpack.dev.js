4943971bec8583ad7c4385cc1ada817a204771e3
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2020-12-25 23:03:21 +0700

    Share vars js <--> scss, Use nodemon to detect webpack config change

--- frontend/webpack/webpack.dev.js
+++ frontend/webpack/webpack.dev.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const sass = require("node-sass");
+const sassUtils = require("node-sass-utils")(sass);
+const sassVars = require('../src/app/style/theme.ts');
 const {
   prod_Path,
   src_Path
@@ -11,6 +13,10 @@ const {
 } = require('./loader');
 
 module.exports = {
+  devServer: {
+		open: true,
+    stats: 'errors-only'
+	},
   entry: {
     main: './' + src_Path + '/index.ts'
   },
@@ -23,7 +29,7 @@ module.exports = {
   },
   devtool: 'source-map',
   devServer: {
-    open: true,
+    open: false,
   },
   module: {
     rules: [{
@@ -51,8 +57,20 @@ module.exports = {
         {
           loader: selectedPreprocessor.loaderName,
           options: {
-            sourceMap: true
-          }
+            sourceMap: true,
+            functions: {
+              "get($keys)": function(keys) {
+                keys = keys.getValue().split(".");
+                let result = sassVars;
+                let i;
+                for (i = 0; i < keys.length; i++) {
+                  result = result[keys[i]];
+                }
+                result = sassUtils.castToSass(result);
+                return result;
+              }
+            }
+          },
         },
       ]
     }]
