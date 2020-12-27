685e72046d92b08a8a7ed621a83b98c13c0bf257
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2020-12-27 15:39:56 +0700

    Fix path for mobile app (Cherry picked)

--- frontend/src/app/const/path.ts
+++ frontend/src/app/const/path.ts
@@ -1,2 +1,5 @@
+// Change "IS_MOBILE_APP_BUILD" to true when deploy production to mobile app
+const IS_MOBILE_APP_BUILD = true
+
 export const HOST_SIGNALR = "http://192.168.1.9:5000/lotohub"
-export const STATIC = '/assets'
+export const STATIC = (IS_MOBILE_APP_BUILD) ? 'file:///android_asset/www/assets' : '/assets'
