0c06e2e34abdb9e63b6ceb900b3a5d27b4b4b979
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2020-12-27 15:36:44 +0700

    Fix path for mobile app

--- frontend/src/app/const/audiopath.ts
+++ frontend/src/app/const/audiopath.ts
@@ -1,4 +1,5 @@
-const PREFIX = '/assets/'
-export const GOOGLE_VOICE_NUMBER_PATH = PREFIX + 'googlevoicenumber/'
-export const CLICK = PREFIX + 'click.mp3'
-export const YAY = PREFIX + 'yay.mp3'
+import {STATIC} from './path'
+const PREFIX = STATIC
+export const GOOGLE_VOICE_NUMBER_PATH = PREFIX + '/googlevoicenumber/'
+export const CLICK = PREFIX + '/click.mp3'
+export const YAY = PREFIX + '/yay.mp3'
