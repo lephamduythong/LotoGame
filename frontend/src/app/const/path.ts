364c1dff2f846d5322fc112787c67b9e66e1efcf
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2020-12-28 11:28:26 +0700

    Fix path and Clean signalR files

--- frontend/src/app/const/path.ts
+++ frontend/src/app/const/path.ts
@@ -1,5 +1,1 @@
-// Change "IS_MOBILE_APP_BUILD" to true when deploy production to mobile app
-const IS_MOBILE_APP_BUILD = true
-
-export const HOST_SIGNALR = "http://localhost:5000/lotohub"
-export const STATIC = (IS_MOBILE_APP_BUILD) ? 'file:///android_asset/www/assets' : '/assets'
+export const STATIC = 'assets'
--- frontend/src/app/wscallback.ts
+++
@@ -1,8 +0,0 @@
-import { connection } from './wsconnection'
-
-export async function activeCallbacksAsync() {
-    connection.on("TestCallback", (result) => {
-        console.log(result)
-    })
-}
-
