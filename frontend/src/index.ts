0fee3da113bed9b7a66e205a67af9282234c523c
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2020-12-31 18:13:01 +0700

    Dynamically add CSS

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,29 +1,23 @@
 import './index.scss';
-import anime from 'animejs';
 
-/*
-Modifier (flag):
-    g: global match, match toàn bộ không chỉ first
-    i: case-insensitive, không phân biệt hoa hay thường
-    m: multiline matching, dùng khi str có kí tự \n phân cách
-*/
+let dynamicStyles: HTMLStyleElement = null;
 
-/* 
-Method:
-string
-    .search(pattern): return index, if no matched return -1
-    .match(pattern): trả về chuỗi khớp với pattern !importaint
-    .test(): true or false
-    .replace(pattern, str_to_replace)
-regex
-    .exec(string):
-*/
+function addAnimation(rules: string) {
+  if (!dynamicStyles) {
+    dynamicStyles = document.createElement('style');
+    document.head.appendChild(dynamicStyles);
+  }
+  dynamicStyles.sheet.insertRule(rules);
+}
 
-// Named group: ?<name>
+addAnimation(`
+  @keyframes myAnimation { 
+    0% { transform: rotate(0); }
+    20% { transform: rotate(${ 360 * Math.random() }deg); }
+    60% { transform: rotate(${ -360 * Math.random() }deg); }
+    90% { transform: rotate(${ 360 * Math.random() }deg); }
+    100% { transform: rotate(${ 0 }deg); }
+  }
+`);
 
-let str = `<img data-visualcompletion="media-vc-image" src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.0-9/135012986_2678251229154814_3233954408280557016_n.jpg?_nc_cat=101&amp;ccb=2&amp;_nc_sid=825194&amp;_nc_ohc=GhBuBnV9ju8AX-RV7HH&amp;_nc_ht=scontent.fsgn2-4.fna&amp;oh=a816f2141f9d0b64fcd15cddc5c31c42&amp;oe=601227A8" alt="Hình ảnh có thể có: văn bản" class="ji94ytn4 r9f5tntg d2edcug0 r0294ipz" data-other="Hihi Haha Hoho">`
-
-const imgLinkGroup = 'imgLink'
-let regex = new RegExp(`src="(?<${imgLinkGroup}>https://\\S+)"`, "i")
-let result = str.match(regex).groups
-console.log(result)
+document.getElementById("circle").style.animation = 'myAnimation 3s';
