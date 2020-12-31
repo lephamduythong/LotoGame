47a686763aa998ce0e1428b2c015d187eeee4c4b
THONGLEE\Administrator <lephamduythong2296@gmail.com>
2020-12-31 17:59:04 +0700

    VCC

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -10,17 +10,18 @@ Modifier (flag):
 
 /* 
 Method:
-str
-    .search(pattern): true or false
+string
+    .search(pattern): return index, if no matched return -1
     .match(pattern): trả về chuỗi khớp với pattern !importaint
-    .test(): 
-    .exec():
+    .test(): true or false
     .replace(pattern, str_to_replace)
+regex
+    .exec(string):
 */
 
 // Named group: ?<name>
 
-let str = `<img data-visualcompletion="media-vc-image" alt="Hình ảnh có thể có: văn bản" class="ji94ytn4 r9f5tntg d2edcug0 r0294ipz" src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.0-9/135012986_2678251229154814_3233954408280557016_n.jpg?_nc_cat=101&amp;ccb=2&amp;_nc_sid=825194&amp;_nc_ohc=GhBuBnV9ju8AX-RV7HH&amp;_nc_ht=scontent.fsgn2-4.fna&amp;oh=a816f2141f9d0b64fcd15cddc5c31c42&amp;oe=601227A8">`
+let str = `<img data-visualcompletion="media-vc-image" src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.0-9/135012986_2678251229154814_3233954408280557016_n.jpg?_nc_cat=101&amp;ccb=2&amp;_nc_sid=825194&amp;_nc_ohc=GhBuBnV9ju8AX-RV7HH&amp;_nc_ht=scontent.fsgn2-4.fna&amp;oh=a816f2141f9d0b64fcd15cddc5c31c42&amp;oe=601227A8" alt="Hình ảnh có thể có: văn bản" class="ji94ytn4 r9f5tntg d2edcug0 r0294ipz" data-other="Hihi Haha Hoho">`
 
 const imgLinkGroup = 'imgLink'
 let regex = new RegExp(`src="(?<${imgLinkGroup}>https://\\S+)"`, "i")
